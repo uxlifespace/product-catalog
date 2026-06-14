afc412f39bc22d614a5dfb484e38d2a13bea1db1
uxlifespace <ux.life.space@gmail.com>
2026-06-14 23:07:13 +0300

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -2,7 +2,7 @@
 // Docs: https://api.monobank.ua/docs/acquiring.html
 // Token: MONOBANK_TOKEN env variable (merchant token from portal.monobank.ua)
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   const { orderId, amount, items } = req.body || {};
